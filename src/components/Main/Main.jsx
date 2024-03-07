4684859d84ec51ecb8419255074c26faadbe4e54
Manmeet <manmeetnagiii@gmail.com>
2024-03-07 18:37:15 +0530

    fixing scrollbar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -24,9 +24,30 @@ const Main = () => {
       </div>
 
       <div className="main-container">
-        {!showResult ? (
+        {showResult ? (
+          <div className="result">
+          <div className="result-title">
+            <img src={assets.user_icon} alt="" />
+            <p>{recentPrompt}</p>
+          </div>
+
+          <div className="resultData">
+            <img src={assets.gemini_icon} alt="" />
+            {loading ? (
+              <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+              </div>
+            ) : (
+              <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+            )}
+          </div>
+        </div>
+            
+          
+        ) : (
           <>
-            {" "}
             <div className="greet">
               <p>
                 <span>Hello, Dev.</span>
@@ -51,27 +72,7 @@ const Main = () => {
                 <img src={assets.code_icon} alt="" />
               </div>
             </div>
-          </>
-        ) : (
-          <div className="result">
-            <div className="result-title">
-              <img src={assets.user_icon} alt="" />
-              <p>{recentPrompt}</p>
-            </div>
-
-            <div className="resultData">
-              <img src={assets.gemini_icon} alt="" />
-              {loading ? (
-                <div className="loader">
-                    <hr />
-                    <hr />
-                    <hr />
-                </div>
-              ) : (
-                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
-              )}
-            </div>
-          </div>
+            </>
         )}
 
         <div className="main-bottom">
